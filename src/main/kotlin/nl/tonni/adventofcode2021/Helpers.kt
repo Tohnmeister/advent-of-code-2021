84fb03b2dee8e7b5cb7174f1e00526cbd5a24223
Tonni Tielens <tonni@tonni.nl>
2021-12-04 12:44:00 +0100

    Add Helpers

---
+++ src/main/kotlin/nl/tonni/adventofcode2021/Helpers.kt
@@ -0,0 +1,6 @@
+package nl.tonni.adventofcode2021
+
+import java.io.File
+
+fun readLinesToInts(pathname: String) = readLines(pathname).map { it.toInt() }
+fun readLines(pathname: String) = File(pathname).readLines()
