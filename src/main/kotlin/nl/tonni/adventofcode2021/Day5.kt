23738f5fa98ba1912254f4425629c5d1db489db2
Tonni Tielens <tonni@tonni.nl>
2021-12-06 22:12:54 +0100

    And make it slightly more readable

--- src/main/kotlin/nl/tonni/adventofcode2021/Day5.kt
+++ src/main/kotlin/nl/tonni/adventofcode2021/Day5.kt
@@ -3,14 +3,25 @@ package nl.tonni.adventofcode2021
 import kotlin.math.min
 import kotlin.math.max
 
-private data class Point(val x: Int, val y: Int)
+private data class Point(val x: Int, val y: Int) {
+
+}
 private data class Line(val start: Point, val end: Point) {
     val minX = min(start.x, end.x)
     val maxX = max(start.x, end.x)
     val minY = min(start.y, end.y)
     val maxY = max(start.y, end.y)
-    val allPoints = (minX..maxX).flatMap { x -> (minY..maxY).map { y -> Point(x, y) }}
     val isStraight = start.x == end.x || start.y == end.y
+    val allPoints = if (isStraight) (minX..maxX).flatMap { x -> (minY..maxY).map { y -> Point(x, y) }} else
+        getIntProgression(start.x, end.x).zip(getIntProgression(start.y, end.y)) { x, y -> Point(x, y) }
+
+    private fun getIntProgression(start: Int, end: Int): IntProgression {
+        return if (start < end) {
+            start..end
+        } else {
+            start downTo end
+        }
+    }
 }
 
 private fun createLine(lineStr: String): Line {
@@ -29,7 +40,7 @@ private fun createPoint(pointStr: String): Point {
 }
 
 fun main() {
-    val lines = readLines("input/day5/input.txt").map(::createLine).filter { it.isStraight }
+    val lines = readLines("input/day5/input.txt").map(::createLine)
 
     val coveredPointCount = lines.fold(emptyMap<Point, Int>()) { accCoveredPointCount, line ->
         val newPointCount = line.allPoints.associateWith { point -> accCoveredPointCount.getOrDefault(point, 0) + 1 }
