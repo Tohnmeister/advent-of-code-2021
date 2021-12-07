d7378f9755bdec18575c3e70a93d57ebcf0e4dce
Tonni Tielens <tonni@tonni.nl>
2021-12-07 17:18:06 +0100

    Use histogram to count occurrences of all lantern fish with a given internal timer

--- src/main/kotlin/nl/tonni/adventofcode2021/Day6.kt
+++ src/main/kotlin/nl/tonni/adventofcode2021/Day6.kt
@@ -13,8 +13,28 @@ tailrec fun produceLanternFish(lanternFish: List<Int>, nrOfRemainingDays: Int): 
     return produceLanternFish(existingLanternFish + newLanternFish, nrOfRemainingDays - 1)
 }
 
+tailrec fun produceLanternFish(lanternFishHistogram: Map<Int, Long>, nrOfRemainingDays: Int): Map<Int, Long> {
+    if (nrOfRemainingDays == 0) {
+        return lanternFishHistogram
+    }
+
+    val newLanternFishHistogram = mapOf(
+        8 to lanternFishHistogram.getOrDefault(0, 0),
+        7 to lanternFishHistogram.getOrDefault(8, 0),
+        6 to lanternFishHistogram.getOrDefault(7, 0) + lanternFishHistogram.getOrDefault(0, 0),
+        5 to lanternFishHistogram.getOrDefault(6, 0),
+        4 to lanternFishHistogram.getOrDefault(5, 0),
+        3 to lanternFishHistogram.getOrDefault(4, 0),
+        2 to lanternFishHistogram.getOrDefault(3, 0),
+        1 to lanternFishHistogram.getOrDefault(2, 0),
+        0 to lanternFishHistogram.getOrDefault(1, 0),
+    )
+
+    return produceLanternFish(newLanternFishHistogram, nrOfRemainingDays - 1)
+}
+
 fun main() {
     val lanternFish = readLine("input/day6/input.txt").split(',').map { it.toInt() }
-
-    println(produceLanternFish(lanternFish, 256).size)
+    val lanternFishHistogram = lanternFish.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+    println(produceLanternFish(lanternFishHistogram, 256).values.sum())
 }
